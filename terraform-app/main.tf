72b16bd3b08c1cbab3b2a05f0af78bcb5f100061
Fardeen <Fardeen.2812@gmail.com>
2025-11-21 00:59:23 +0530

    added monitroing and HPA

--- terraform-app/main.tf
+++ terraform-app/main.tf
@@ -32,7 +32,8 @@ module "vpc" {
   private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  single_nat_gateway = true
+  enable_vpn_gateway = false
 }
 
 # Define the worker node group inside the eks module
@@ -42,19 +43,32 @@ module "eks" {
   version         = "~> 20.0"
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+  }
+
   eks_managed_node_group_defaults = {
-    instance_types = ["t3.medium"]
+    instance_types = ["t3.small"]
+    iam_role_additional_policies = {
+      AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    }
   }
 
   eks_managed_node_groups = {
     main = {
       name         = var.node_group_name
-      min_size     = 2
+      min_size     = 1
       max_size     = 3
       desired_size = 2
+
+      capacity_type = "SPOT"
     }
   }
 }
